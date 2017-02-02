a48f62bb29435ad8cb60ce0e4cb872dc5b1d7507
therealphoenix <fennya@gmail.com>
2017-02-02 18:39:31 +0200

    input in Runner

--- src/main/java/com/klindziuk/traingle/Triangle.java
+++ src/main/java/com/klindziuk/traingle/Triangle.java
@@ -11,9 +11,9 @@ public class Triangle {
 	private int sideC;
 	
 	
-	public Triangle() {
+	public Triangle(int[] sideArray) {
 	
-	int [] sideArray = inputSides();	
+		
 	
 		
 		this.sideA = sideArray[0];
@@ -47,54 +47,6 @@ public class Triangle {
 		return sideC;
 	}
 	
-	public int[] inputSides(){
-		
-      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-      int arrayOfsides[] = new int [3];
-		
-		while(true) {
-			
-		System.out.println("Please input sides of triangle, they should be greater then 0 ! ");
-		
-			
-		try	 {
-			
-		System.out.print("Please input first side: ");
-		int a = Integer.parseInt(reader.readLine());
-		
-		System.out.print("Please second side: ");
-		int b = Integer.parseInt(reader.readLine());
-		
-		System.out.print("Please input third side: ");
-		int c = Integer.parseInt(reader.readLine());
-		System.out.println();
-		
-		if((SideChecker.CheckSides(a, b, c) && SideChecker.checkSumOfSides(a,b,c))){
-			System.out.println("Sides are correct, we can create triangle.");
-			
-			this.sideA = a;
-			this.sideB = b;
-			this.sideC = c;
-			
-			reader.close();
-			break;
-		}
-		 System.out.println("Sides are incorrect, we can't create triangle, please try again:");
-		 System.out.println();
-		}
-		
-		 catch (NumberFormatException ex){
-		
-			System.out.println("Only numbers allowed, please try again:");
-		 }
-		
-		catch ( IOException ioex) {
-			ioex.printStackTrace();
-		}
-		}
-		
-		return arrayOfsides;
-	}
 	
 	public boolean isScaleneTriangle(int sideA, int sideB, int sideC) {
 		
