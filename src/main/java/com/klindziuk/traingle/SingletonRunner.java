6d68de3790f4a3e5234800ac36bd07a32f5fb006
therealphoenix <fennya@gmail.com>
2017-02-03 11:20:33 +0200

    almost Singleton :D

---
+++ src/main/java/com/klindziuk/traingle/SingletonRunner.java
@@ -0,0 +1,57 @@
+package com.klindziuk.traingle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SingletonRunner {
+
+	public static void main(String[] args) {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+
+			System.out.println("Please input sides of triangle, they should be greater then 0 ! ");
+
+			try {
+
+				System.out.print("Please input first side: ");
+				int a = Integer.parseInt(reader.readLine());
+
+				System.out.print("Please second side: ");
+				int b = Integer.parseInt(reader.readLine());
+
+				System.out.print("Please input third side: ");
+				int c = Integer.parseInt(reader.readLine());
+				System.out.println();
+
+				if ((SideChecker.CheckSides(a, b, c) && SideChecker.checkSumOfSides(a, b, c))) {
+					System.out.println("Sides are correct, we can create triangle.");
+
+					SingletonTriangle.init(a, b, c);
+					reader.close();
+					break;
+
+				}
+				System.out.println("Sides are incorrect, we can't create triangle, please try again:");
+				System.out.println();
+			}
+
+			catch (NumberFormatException ex) {
+
+				System.out.println("Only numbers allowed, please try again:");
+			}
+
+			catch (IOException ioex) {
+				ioex.printStackTrace();
+			}
+
+		}
+
+		SingletonTriangle triangle = SingletonTriangle.getInstance();
+		triangle.decideTriangleType(triangle);
+
+	}
+
+}
---
+++ src/main/java/com/klindziuk/traingle/SingletonTriangle.java
@@ -0,0 +1,85 @@
+package com.klindziuk.traingle;
+
+public class SingletonTriangle {
+
+	private static SingletonTriangle singletonTriangle;
+
+	private int sideA;
+	private int sideB;
+	private int sideC;
+
+	public int getSideA() {
+		return sideA;
+	}
+
+	public int getSideB() {
+		return sideB;
+	}
+
+	public int getSideC() {
+		return sideC;
+	}
+
+	private SingletonTriangle(int sideA, int sideB, int sideC) {
+		this.sideA = sideA;
+		this.sideA = sideB;
+		this.sideA = sideC;
+	}
+
+	public static void init(int sideA, int sideB, int sideC) {
+		singletonTriangle = null;
+		singletonTriangle = new SingletonTriangle(sideA, sideB, sideC);
+
+	}
+
+	public static SingletonTriangle getInstance() {
+		return singletonTriangle;
+	}
+
+	public boolean isScaleneTriangle(int sideA, int sideB, int sideC) {
+
+		if (((sideA + sideB) > sideC) && ((sideB + sideC) > sideA) && ((sideA + sideC) > sideB) && (sideA != sideB)
+				&& (sideA != sideC) && (sideB != sideC))
+
+		{
+
+			System.out.println("Triangle is Scalene and correct.");
+			return true;
+		} else
+
+			return false;
+	}
+
+	public boolean isEquilateralTriangle(int sideA, int sideB, int sideC) {
+
+		if ((sideA == sideB) && (sideA == sideC) && (sideB == sideC)) {
+
+			System.out.println("Triangle is Equilateral and correct.");
+			return true;
+		} else
+			return false;
+
+	}
+
+	public boolean isIsoscelesTriangle(int sideA, int sideB, int sideC) {
+
+		if ((sideA == sideB) || (sideB == sideC) || (sideA == sideC)) {
+
+			System.out.println("Triangle is Isosceles and correct.");
+			return true;
+		}
+
+		else
+			return false;
+
+	}
+
+	public void decideTriangleType(SingletonTriangle triangle) {
+
+		isIsoscelesTriangle(triangle.getSideA(), triangle.getSideB(), triangle.getSideC());
+		isEquilateralTriangle(triangle.getSideA(), triangle.getSideB(), triangle.getSideC());
+		isScaleneTriangle(triangle.getSideA(), triangle.getSideB(), triangle.getSideC());
+
+	}
+
+}
