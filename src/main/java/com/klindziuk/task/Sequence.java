fd8e9ea2c9315453589f0d8be1ec8d6890334c14
therealphoenix <fennya@gmail.com>
2017-02-09 02:22:49 +0200

    last

--- src/main/java/com/klindziuk/task/Sequence.java
+++ src/main/java/com/klindziuk/task/Sequence.java
@@ -7,62 +7,59 @@ package com.klindziuk.task;
  * 
  */
 
-
-
 public class Sequence {
 
 	public static void main(String[] args) {
-		
 
 		System.out.println(decideSequence(args[0]));
 
 	}
 
 	public static String decideSequence(String number) {
-		
+
 		char[] charArray = number.toCharArray();
-			
-			String result = "";
 
-			for (int j = 0; j < charArray.length - 1; j++) {
+		String result = "";
 
-				if (charArray[j] > charArray[j+1]) {
+		for (int j = 0; j < charArray.length - 1; j++) {
 
-					result = number + " - Sequence is not growing.";
-					break;
-				} else {
+			if (charArray[j] > charArray[j + 1]) {
 
-					result = number + " - Sequence is growing.";
-				}
+				result = number + " - Sequence is not growing.";
+				break;
+			} else {
 
+				result = number + " - Sequence is growing.";
 			}
 
-			return result;
-	}
-		
-		
-//		int firstNumber = Integer.parseInt(String.valueOf(array[0]));
-//		int secondNumber = Integer.parseInt(String.valueOf(array[1]));
-//		int preLastNumber = Integer.parseInt(String.valueOf(array[array.length - 2]));
-//		int lastNumber = Integer.parseInt(String.valueOf(array[array.length - 1]));
-//		
-//		System.out.println(firstNumber);
-//		System.out.println(secondNumber);
-//		System.out.println(preLastNumber);
-//		System.out.println(lastNumber);
-//
-//		if((firstNumber < secondNumber) && (lastNumber > preLastNumber)) {
-//
-//			System.out.println("Sequence is growing");
-//				
-//			}
-//		
-//		else {
-//
-//				System.out.println ("Sequence is not  growing");
-//			}
-//
-//		
+		}
 
+		return result;
 	}
 
+	// int firstNumber = Integer.parseInt(String.valueOf(array[0]));
+	// int secondNumber = Integer.parseInt(String.valueOf(array[1]));
+	// int preLastNumber = Integer.parseInt(String.valueOf(array[array.length -
+	// 2]));
+	// int lastNumber = Integer.parseInt(String.valueOf(array[array.length -
+	// 1]));
+	//
+	// System.out.println(firstNumber);
+	// System.out.println(secondNumber);
+	// System.out.println(preLastNumber);
+	// System.out.println(lastNumber);
+	//
+	// if((firstNumber < secondNumber) && (lastNumber > preLastNumber)) {
+	//
+	// System.out.println("Sequence is growing");
+	//
+	// }
+	//
+	// else {
+	//
+	// System.out.println ("Sequence is not growing");
+	// }
+	//
+	//
+
+}
